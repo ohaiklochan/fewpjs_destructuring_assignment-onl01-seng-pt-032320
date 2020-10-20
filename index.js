d77d985975765b42b7480e434f365fca5d535b1a
Chloe Steffens <caforonda103@yahoo.com>
2020-10-20 06:11:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const farmAnimals [moo, neigh, baa, oink, cluck] = 'cow horse sheep pig chicken'.split(" ")
+const farmAnimals = ['cow horse sheep pig chicken'
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
